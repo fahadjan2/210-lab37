562ac8285a8b706ef3ac0d5d2f89ec6575a84312
fahadjan2 <fahadfawad2006@gmail.com>
2025-12-06 17:29:43 -0800

    Milestone III

--- code.cpp
+++ code.cpp
@@ -1,15 +1,19 @@
+// COMSC-210 | Lab 37 | Fahad Fawad Ahmad 
+// IDE used: Visual Studio
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <list>
 using namespace std;
 
+const int NUM_DISPLAY = 100;
+
 int gen_hash_index(string);
-void display(map <int, list<string>>);
+void display(map <int, list<string>>&, int);
 
 int main() {
     map <int, list<string>> hash_table;
-
+    
     //File open
 	ifstream file;
 	file.open("data.txt");
@@ -24,15 +28,9 @@ int main() {
         hash_table[hashindex].push_back(line);
     }
     
+    display(hash_table, NUM_DISPLAY);
     return 0;
 }
-/*
-These targets are present in the dataset and can be used for testing:
-536B9DFC93AF
-1DA9D64D02A0
-666D109AA22E
-E1D2665B21EA
-*/
 
 int gen_hash_index(string str) {
     int sum = 0;
@@ -42,17 +40,20 @@ int gen_hash_index(string str) {
     return sum;
 }
 
-void display(map <int, list<string>> data, int numOfEntries) {
-    int count = 1;
+void display(map <int, list<string>> &data, int numOfEntries) {
+    int count = 0;
     for (auto pair : data) { //Loops through each map data
-        cout << "Hash index: " << pair.first; 
-        for (auto value : pair.second) { //Loops through the list
-
+        if (count == numOfEntries) {
+            cout << "End" << endl;
+            break;
         }
-
-        if (count == numOfEntries)
-            return;
         count++;
+        
+        cout << "Hash index: " << pair.first << endl; 
+        for (auto value : pair.second) { //Loops through the list
+            cout << "\t" << value << endl;
+        }
     }
+
     cout << endl;
 }
